3ed0c377236934d5327d960b9589446afbc90cb2
smjeong88 <smvtr123@naver.com>
2021-09-01 00:29:28 +0900

    숫자야구 수정

--- 3.숫자야구/main.js
+++ 3.숫자야구/main.js
@@ -1,3 +1,8 @@
+/**
+ * 1) main.js 파일이 웹팩 파일을 만드는데 기준이 됨
+ * 2) vue 환경에서는 import , node 환경에서는 require
+ * 
+ */
 import Vue from 'vue';
 import NumberBaseball from './NumberBaseball';
 
--- 3.숫자야구/webpack.config.js
+++ 3.숫자야구/webpack.config.js
@@ -1,12 +1,20 @@
+/* 
+웹 팩 : 스크립트 파일이 많아서 하나로 합치는 작업
+*/ 
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
+  // mode : development(개발)
+  //        production(운영)
   devtool: 'eval',
+  // eval(개발)
+  // hidden-source-map(운영)
   resolve: {
     extensions: ['.js', '.vue'],
   },
+  // entry, module, plugins, output 기 기본 뼈대
   entry: {
     app: path.join(__dirname, 'main.js'),
   },
